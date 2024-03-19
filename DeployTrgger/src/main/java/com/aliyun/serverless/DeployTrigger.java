2f6b12c09816fd33deee631c0d33e8451424947d
minghaoli0108 <33147758+minghaoli0108@users.noreply.github.com>
2024-03-19 19:31:15 +0800

    Update DeployTrigger.java
            // String imageUrl = String.format("acr-20231204-registry-vpc.%s.aliyuncs.com/%s:%s",
            String imageUrl = String.format("acr-20231204-registry.%s.aliyuncs.com/%s:%s",

--- DeployTrgger/src/main/java/com/aliyun/serverless/DeployTrigger.java
+++ DeployTrgger/src/main/java/com/aliyun/serverless/DeployTrigger.java
@@ -81,7 +81,9 @@ public class DeployTrigger implements HttpRequestHandler {
             return "";
         }
         // China mainland
-        String imageUrl = String.format("acr-20231204-registry-vpc.%s.aliyuncs.com/%s:%s",
+        // String imageUrl = String.format("registry-vpc.%s.aliyuncs.com/%s:%s",
+        // String imageUrl = String.format("acr-20231204-registry-vpc.%s.aliyuncs.com/%s:%s",
+        String imageUrl = String.format("acr-20231204-registry.%s.aliyuncs.com/%s:%s",
                 payload.getRepository().getRegion(),
                 payload.getRepository().getRepoFullName(),
                 payload.getPushData().getTag());
