e02f6ab3e6dc309729f94cf1fbc18700b76f8e43
minghaoli0108 <33147758+minghaoli0108@users.noreply.github.com>
2024-03-19 19:40:31 +0800

    Update DeployTrigger.java
    edit imagesurl

--- DeployTrgger/src/main/java/com/aliyun/serverless/DeployTrigger.java
+++ DeployTrgger/src/main/java/com/aliyun/serverless/DeployTrigger.java
@@ -82,7 +82,6 @@ public class DeployTrigger implements HttpRequestHandler {
         }
         // China mainland
         // String imageUrl = String.format("registry-vpc.%s.aliyuncs.com/%s:%s",
-        // String imageUrl = String.format("acr-20231204-registry-vpc.%s.aliyuncs.com/%s:%s",
         String imageUrl = String.format("acr-20231204-registry.%s.aliyuncs.com/%s:%s",
                 payload.getRepository().getRegion(),
                 payload.getRepository().getRepoFullName(),
